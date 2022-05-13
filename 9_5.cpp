02546b6b2367dd7c257075aaee1be01185b9ed96
hiranj-kotak <95754714+hiranj-kotak@users.noreply.github.com>
2022-05-13 13:09:52 +0530

    commit

--- 9_5.cpp
+++ 9_5.cpp
@@ -11,17 +11,55 @@ public:
         cout << avg << endl;
     }
 };
-class car
+class car : public vehicle
 {
     string fuel;
-    int no_Of_wheel;
+    int no_of_wheel;
 
 public:
-    void get_data()
+    void getdata()
     {
+        cin >> fuel >> no_of_wheel;
+    }
+    void put_data()
+    {
+        cout << fuel << no_of_wheel;
+    }
+};
+class truck : public vehicle
+{
+    string fuel;
+    int no_of_wheel;
+
+public:
+    void getdata()
+    {
+        cin >> fuel >> no_of_wheel;
+    }
+    void put_data()
+    {
+        cout << fuel << no_of_wheel;
     }
 };
 int main()
 {
+    car c[2];
+    truck t[2];
+    for (int i = 0; i < 2; i++)
+    {
+        c[i].getdata();
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        t[i].getdata();
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        c[i].put_data();
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        t[i].put_data();
+    }
     return 0;
 }
